019e70b47bc37c74c8c42de4908d23cd29898d0d
dragosbus <dragosbusuioc94@gmail.com>
2018-04-01 09:46:04 +0200

    Initialise errors js file

--- js/errors.js
+++ js/errors.js
@@ -1,5 +1,11 @@
 class ERRORS {
   constructor() {
-    
+    this.parentIntro = document.getElementById('intro-page');
+  }
+  errorCategory() {
+    //show error if category is not selected
+  }
+  errorDifficulty() {
+    //show error if difficulty is not select
   }
 }
