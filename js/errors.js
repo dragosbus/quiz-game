410fd0e04bba59db91c6aa187b9f47840e90606c
Dragos Busuioc <dragosbusuioc94@gmail.com>
2018-04-05 09:24:03 +0000

    Added error handler for quizes not existent

--- js/errors.js
+++ js/errors.js
@@ -2,5 +2,11 @@ class ERRORS {
   constructor() {
     this.parentIntro = document.getElementById('intro-page');
   }
-  
+  quizNotAvaible() {
+    let error = `<div class="not-quiz slide-left-in">
+    <h3>Error!</h3>
+    <p>Is not exist questions for category selected!Please choose other category or other difficulty</p>
+    </div>`;
+    this.parentIntro.innerHTML+=error;
+  }
 }
--- js/main.js
+++ js/main.js
@@ -14,91 +14,96 @@
       playerScore = 0,
       indexQuestion = 0,
       quizes = [];
-
-  //append all data when document is loaded
-  document.addEventListener('DOMContentLoaded', () => {
-    ui.categories();
-  });
-
-  //get dificulty clicked
-  (function getDifificulty() {
-    ulChecks.addEventListener('click', e => {
-      let target = e.target;
-      if (target.tagName === 'LABEL') {
-        let value = target.textContent;
-
-        difficultyVal = value.toLowerCase();
-      }
-    });
-  }());
-
-  //get category selected
-  (function getCategory() {
-    selectCategories.addEventListener('change', e => {
-      let target = e.target;
-      let value = target.value;
-      categoryVal = value;
-    });
-  }());
-
-  //fetch data
-  const getData = () => {
-    //get category and difficulty
-    let category = categoryVal;
-    let difficulty = difficultyVal;
-    let api = new Quiz(category, difficulty);
-    return api.fetchData();
-  };
-
-  //quiz game engine
-  const loadQuiz = quiz => {
-    let {
-      category,
-      question,
-      incorrect_answers,
-      correct_answer
-    } = quiz;
-    let thisQuestion = ui.quiz(category, question, incorrect_answers, correct_answer);
-
-    return thisQuestion;
-  };
-
-  //change quiz
-  const nextQuiz = (quizes, i=indexQuestion) => {
-    let animQuizPage = new Animations(document.querySelector('.quiz'));
-    if(document.querySelector('.quiz').classList.contains("slide-left-in")) {
-      document.querySelector('.quiz').classList.remove("slide-left-in")
-    }
-    
-    setTimeout(()=>{
       
-      animQuizPage.slideLeftIn();
-      document.querySelector('.quiz').querySelector('h3').innerHTML = quizes[i].category;
-      document.querySelector('.quiz').querySelector('p').innerHTML = quizes[i].question;
-      document.querySelector('.quiz').querySelectorAll('ul li')[0].innerHTML = quizes[i].incorrect_answers[0];
-      document.querySelector('.quiz').querySelectorAll('ul li')[1].innerHTML = quizes[i].incorrect_answers[1];
-      document.querySelector('.quiz').querySelectorAll('ul li')[2].innerHTML = quizes[i].incorrect_answers[2];
-      document.querySelector('.quiz').querySelectorAll('ul li')[3].innerHTML = quizes[i].correct_answer;
-    },3000);
-  };
+//initialise global variables at initial state
+const init = () => {
+  categoryVal = '';
+  difficultyVal = '';
+  thisQuestion;
+  playerScore = 0;
+  indexQuestion = 0;
+  quizes = [];
+};
+
+//get dificulty clicked
+const getDifificulty = () => {
+  ulChecks.addEventListener('click', e => {
+    let target = e.target;
+    if (target.tagName === 'LABEL') {
+      let value = target.textContent;
+
+      difficultyVal = value.toLowerCase();
+    }
+  });
+};
+
+//get category selected
+const getCategory = () => {
+  selectCategories.addEventListener('change', e => {
+    let target = e.target;
+    let value = target.value;
+    categoryVal = value;
+  });
+};
+
+//fetch data
+const getData = () => {
+  //get category and difficulty
+  let category = categoryVal;
+  let difficulty = difficultyVal;
+  let api = new Quiz(category, difficulty);
+  return api.fetchData();
+};
+
+//quiz game engine
+const loadQuiz = quiz => {
+  let {
+    category,
+    question,
+    incorrect_answers,
+    correct_answer
+  } = quiz;
+  let thisQuestion = ui.quiz(category, question, incorrect_answers, correct_answer);
+
+  return thisQuestion;
+};
+
+//change quiz
+const nextQuiz = (quizes, i=indexQuestion) => {
+  let animQuizPage = new Animations(document.querySelector('.quiz'));
+  if(document.querySelector('.quiz').classList.contains("slide-left-in")) {
+    document.querySelector('.quiz').classList.remove("slide-left-in")
+  }
+    
+  setTimeout(()=>{
+      
+    animQuizPage.slideLeftIn();
+    document.querySelector('.quiz').querySelector('h3').innerHTML = quizes[i].category;
+    document.querySelector('.quiz').querySelector('p').innerHTML = quizes[i].question;
+    document.querySelector('.quiz').querySelectorAll('ul li')[0].innerHTML = quizes[i].incorrect_answers[0];
+    document.querySelector('.quiz').querySelectorAll('ul li')[1].innerHTML = quizes[i].incorrect_answers[1];
+    document.querySelector('.quiz').querySelectorAll('ul li')[2].innerHTML = quizes[i].incorrect_answers[2];
+    document.querySelector('.quiz').querySelectorAll('ul li')[3].innerHTML = quizes[i].correct_answer;
+  },3000);
+};
 
   //check fom right answer
-  const checkAnswer = (choice, rightAnswer) => {
+const checkAnswer = (choice, rightAnswer) => {
 
-    function addClass(classTarget) {
-      choice.classList.add(classTarget);
-      setTimeout(() => {
-        choice.classList.remove(classTarget);
-      }, 1200);
-    }
+  function addClass(classTarget) {
+    choice.classList.add(classTarget);
+    setTimeout(() => {
+      choice.classList.remove(classTarget);
+    }, 1200);
+  }
 
-    if (choice.textContent === rightAnswer) {
-      playerScore += 1;
-      addClass('right-answer');
-    } else {
-      addClass('wrong-answer');
-    }
-  };
+  if (choice.textContent === rightAnswer) {
+    playerScore += 1;
+    addClass('right-answer');
+  } else {
+    addClass('wrong-answer');
+  }
+};
   
 const chooseAnswer = e => {
   let t = e.target;
@@ -114,25 +119,49 @@ const chooseAnswer = e => {
   }
 };
 
+const handlerQuizNotExistent = () =>{
+  let quizNotExistent = new ERRORS();
+  quizNotExistent.quizNotAvaible();
+  document.getElementById("intro-page").classList.remove("fade-out");
+  init();
+  //hide error message after 3 seconds
+  setTimeout(()=>{
+    document.getElementById("intro-page").removeChild(document.querySelector(".not-quiz"));
+    // location.reload();
+  }, 3000);
+};
+
+const handlerQuizSuccess = quizes =>{
+  let uiIntro = new Animations(document.getElementById("intro-page"));
+  uiIntro.fadeOut();
+  thisQuestion = loadQuiz(quizes[indexQuestion]);
+  setTimeout(()=>{
+    main.innerHTML = thisQuestion;
+    //start choose answer event
+    document.querySelector('.quiz ul').addEventListener('click', chooseAnswer); //select answer event
+  },550);
+};
+
   //play button event
   const playBtnHandler = () => {
     playBtn.addEventListener('click', e => {
       getData().then(res => quizes = res.results)
         .then(()=> {
-          let uiIntro = new Animations(document.getElementById("intro-page"));
-          uiIntro.fadeOut();
-          thisQuestion = loadQuiz(quizes[indexQuestion]);
-          
-          setTimeout(()=>{
-            main.innerHTML = thisQuestion;
-            //start choose answer event
-            document.querySelector('.quiz ul').addEventListener('click', chooseAnswer); //select answer event
-          },550);
-          
+          if(quizes.length > 0) {
+            handlerQuizSuccess(quizes);
+          } else {
+            handlerQuizNotExistent();
+          }
         });
     });
   };
 
-  playBtnHandler();
+  //append all data when document is loaded
+  document.addEventListener('DOMContentLoaded', () => {
+    ui.categories();
+    getDifificulty();
+    getCategory();
+    playBtnHandler();
+  });
 
 }());
