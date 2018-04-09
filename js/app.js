cf6bf584719c54838eb5a025f4580639ceb3dfbc
Dragos Busuioc <dragosbusuioc94@gmail.com>
2018-04-09 10:44:09 +0000

    added timer and fetchdata handler in its own module°

--- js/app.js
+++ js/app.js
@@ -9,15 +9,39 @@ const App = (function() {
                 let value = target.textContent;
                 this.difficultyVal = value.toLowerCase();
             }
+        },
+        getCategory(e){
+            let target = e.target;
+            let value = target.value;
+            this.categoryVal = value;
+        }
+    };
+    
+    const timer = {
+        timer:0,
+        setTimer(difficulty) {
+            if (difficulty === 'easy') this.timer = 10;
+            else if (difficulty === 'medium') this.timer = 15;
+            else this.timer = 20;
+            
+            return this.timer;
+        },
+        resetTimer() {
+            this.timer = 0;
+            return this.timer;
+        }
+    };
+    
+    const fetchData = {
+        init() {
+            let {category, difficulty} = introPage;
+            this.api = new Quiz(category, difficulty);
+            return this.api.fetchData();
         }
     };
     
     function init() {
         
-    }
-    
-    function timer() {
-         
     }
     
 }());
