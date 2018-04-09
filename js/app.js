dbdecfdb2f85b9fcb03e3c56683449dc25b50a0a
Dragos Busuioc <dragosbusuioc94@gmail.com>
2018-04-09 07:35:42 +0000

    added the intro page handler in its own module

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
 const App = (function() {
     
+    const introPage = {
+        categoryVal:"",
+        difficultyVal:"",
+        getDifificulty(e) {
+            let target = e.target;
+            if (target.tagName === 'LABEL') {
+                let value = target.textContent;
+                this.difficultyVal = value.toLowerCase();
+            }
+        }
+    };
     
+    function init() {
+        
+    }
+    
+    function timer() {
+         
+    }
     
 }());
