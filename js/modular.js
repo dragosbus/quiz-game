0637771a7fcfefb8666bfd8bed35c5e516a13355
Dragos Busuioc <dragosbusuioc94@gmail.com>
2018-04-13 10:14:18 +0000

    BUG:Data is not fetched

--- js/modular.js
+++ js/modular.js
@@ -4,7 +4,7 @@
        quizes: [],
        init(category, difficulty) {
            let api = new Quiz(category, difficulty);
-           api.fetchData().then(res=>this.quizes=res);
+           api.fetchData().then(res=>this.quizes = res);
        },
        getQuiz() {
            return this.quizes;
@@ -16,20 +16,44 @@
            data.init();
            view.init();
        },
-        timer: {
-            time:0,
+       
+       setData(category, difficulty) {
+           data.init(category, difficulty);
+       },
+       
+       getData() {
+         let quiz = data.getQuiz();
+         return quiz;
+       },
+       
+        clock: {
+            timer:0,
             setTimer(difficulty) {
-            if (difficulty === 'easy') this.timer = 10;
-            else if (difficulty === 'medium') this.timer = 15;
-            else this.timer = 20;
+                if (difficulty === 'easy') this.timer = 10;
+                else if (difficulty === 'medium') this.timer = 15;
+                else this.timer = 20;
             
-            return this.timer;
+                return this.timer;
+            },
+            resetTimer() {
+                this.timer = 0;
+                return this.timer;
+            }
         },
-resetTimer() {
-this.timer = 0;
-return this.timer;
-}
-}
+        
+        difficulty:{
+            difficultyVal:"",
+            setDifficultyVal(value) {
+                this.difficultyVal = value;
+            }
+        },
+        
+        category:{
+            categoryVal: "",
+            setCategoryVal(value) {
+                this.categoryVal = value;
+            }
+        },
    };
    
    const view = {
@@ -40,9 +64,13 @@ return this.timer;
             this.playBtn = document.querySelector(".play");
             this.render();
        },
+       
        render() {
           this.categories();
+          this.getCategory();
+          this.playBtnEvent();
        },
+       
        categories() {
             let categories = [["Sports", 21], ["Geography", 22], ["Politics", 24],["History", 23]];
             for (let cat of categories) {
@@ -52,6 +80,32 @@ return this.timer;
             this.selectCat.appendChild(selectOption);
             }
           
+      },
+      
+      getCategory() {
+        this.selectCat.addEventListener('change', e => {
+            let target = e.target;
+            let value = target.value;
+            controller.category.setCategoryVal(value);
+        });
+      },
+      
+      getDifificulty() {
+        this.selectDifficulty.addEventListener('click', e => {
+            let target = e.target;
+            if (target.tagName === 'LABEL') {
+            let value = target.textContent;
+            controller.difficulty.setDifficultyVal(value.toLowerCase());
+            }
+        });  
+      },
+      
+      playBtnEvent() {
+        this.playBtn.addEventListener("click",()=>{
+           controller.setData(controller.difficulty.difficultyVal, controller.category.categoryVal);
+           const quiz = controller.getData();
+           console.log(quiz);
+        });  
       }
    };
    
